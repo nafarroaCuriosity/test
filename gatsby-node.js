7f0c1be81680969a3bdc761f10ea1758e423500f
nafarroaCuriosity <nafarroa.ch32@gmail.com>
2024-03-20 18:51:23 +0900

    mod4

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,40 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
- */
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
-/**
- * @type {import('gatsby').GatsbyNode['createPages']}
- */
-exports.createPages = async ({ actions }) => {
+// exports.onCreateNode = ({ node, getNode, actions }) => {
+//   const { createNodeField } = actions
+
+//   if (node.internal.type === `MarkdownRemark`) {
+//     const slug = createFilePath({ node, getNode })
+//     createNodeField({
+//       node,
+//       name: `slug`,
+//       value: slug
+//     })
+//   }
+// }
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
+
+  const result = await graphql(`
+    query {
+      allContentfulBlog {
+        edges{
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  result.data.allContentfulBlog.edges.forEach(({ node }) => {
+    createPage({
+      path: node.slug,
+      component: path.resolve(`./src/templates/single-blog.js`),
+      context: {
+        slug: node.slug,
+      },
+    })
   })
 }
