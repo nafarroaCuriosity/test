b58df2b2b17e559445d8583145c979878b800a6e
nafarroaCuriosity <nafarroa.ch32@gmail.com>
2024-06-19 00:43:12 +0900

    2024061901

--- src/templates/single-blog.js
+++ src/templates/single-blog.js
@@ -3,9 +3,13 @@ import { graphql } from "gatsby"
 import { GatsbyImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import { CssBaseline, Grid, Typography } from "@mui/material"
+import { useTheme } from "@mui/material/styles"
+import { CssBaseline, Grid, Typography, useMediaQuery } from "@mui/material"
 
 const SingleBlog = (props) => {
+
+  const theme = useTheme()
+  const matches = useMediaQuery(theme.breakpoints.down('md'))  // md:900px
 
   return (
     <>
@@ -15,7 +19,7 @@ const SingleBlog = (props) => {
           title={props.data.contentfulBlog.title}
           description={props.data.contentfulBlog.excerpt}
         />
-        <Grid container maxWidth={'lg'} sx={{ mx: 'auto', mt: 12 }}>
+        <Grid container maxWidth={matches ? 'md' : 'lg'} sx={{ mx: 'auto', mt: 12 }}>
           <Grid item sx={{ mx: 'auto' }}>
             <GatsbyImage
               image={props.data.contentfulBlog.image.gatsbyImageData}
@@ -23,15 +27,15 @@ const SingleBlog = (props) => {
             />
           </Grid>
         </Grid>
-        <Grid cotainer maxWidth={'md'} sx={{ mt: 8, mx: 'auto' }}>
+        <Grid cotainer maxWidth={matches ? 'sm' : 'md'} sx={{ mt: 8, mx: 'auto' }}>
           <Grid item sx={{ mx: 'auto' }}>
-            <Typography variant="h3">{props.data.contentfulBlog.title}</Typography>
+            <Typography variant={matches ? 'h4' : "h3"}>{props.data.contentfulBlog.title}</Typography>
           </Grid>
-          <Grid item>
-            <Typography variant="body2">{props.data.contentfulBlog.date}</Typography>
+          <Grid item textAlign='right'>
+            <Typography variant={matches ? 'caption' : "body2"}>{props.data.contentfulBlog.date}</Typography>
           </Grid>
-          <Grid item sx={{ display:'flex',justifyContent:'center', mt:2 }}>
-            <Typography variant="body1"
+          <Grid item sx={{ display: 'flex', justifyContent: 'center', alignItems:'center', mt: 2 }}>
+            <Typography variant={matches ? 'body2' : "body1"}
               dangerouslySetInnerHTML={{
                 __html: props.data.contentfulBlog.textBody.childMarkdownRemark.html
               }}
